815fbc37e467f9bf31178c465a4539f34af8deee
lixunhuan <2601070799@qq.com>
2020-06-11 14:54:23 +0800

    修改内外循环

--- T1.java
+++ T1.java
@@ -2,8 +2,8 @@ public class T1 {
     /**
      * 利用循环语句，打印一个四行四列的正方形。*/
     public static void main(String[] args) {
-        for (int i = 0; i <5 ; i++) {
-            for (int j = 0; j <4 ; j++) {
+        for (int i = 0; i <7 ; i++) {
+            for (int j = 0; j <7 ; j++) {
                 System.out.print("*"+"\t");
             }
             System.out.println();
