a7304c5adf9cebdfd21fdedef3bb56698bf49c64
lixunhuan <2601070799@qq.com>
2020-06-11 15:05:26 +0800

    修改999

--- T1.java
+++ T1.java
@@ -2,7 +2,7 @@ public class T1 {
     /**
      * 利用循环语句，打印一个四行四列的正方形。*/
     public static void main(String[] args) {
-        for (int i = 0; i <7 ; i++) {
+        for (int i = 0; i <79 ; i++) {
             for (int j = 0; j <7 ; j++) {
                 System.out.print("*"+"\t");
             }
